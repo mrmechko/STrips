0e23fb37d4582308df7a757ec57e6e8751953428
mrmechko <rbose@cs.rochester.edu>
2015-06-20 18:11:05 -0400

    updated tests

--- src/test/scala/TripsTest.scala
+++ src/test/scala/TripsTest.scala
@@ -6,8 +6,9 @@ import org.scalatest.{FlatSpec, Matchers}
  * Created by mechko on 6/19/15.
  */
 class BaseTest extends FlatSpec with Matchers {
-  def f(lemma : String) = println(STripsQuery.findWord(lemma).map(_.name).mkString("%s: [".format(lemma), ", ", "]"))
   def h(lemma : TConcept) = println(STripsQuery.pathToRoot(lemma).map(_.name).mkString("%s: [".format(lemma.name), ", ", "]"))
+  def f(lemma : String) = println(STripsQuery.findWord(lemma).map(_.name).mkString("%s: [".format(lemma), ", ", "]"))
+  def p(lemma : String) = println(STripsQuery.findWordBySense(lemma).map(r=> "sense:\n\t%s\ttrips\t%s".format(r._1.id, r._2.name)).mkString("%s:".format(lemma), "\n  ", "]"))
 
   LoadTrips()
 
@@ -22,6 +23,14 @@ class BaseTest extends FlatSpec with Matchers {
     f("run")
     f("fight")
     f("kill")
+  }
+
+  "a trips search" should "be able to return sense pairs" in {
+    p("cat")
+    p("dog")
+    p("run")
+    p("fight")
+    p("kill")
   }
 
   "a trips concept" should "head to the root" in {
