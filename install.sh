4e46212a0f6466278b6b9bb47ef200947e74f8bb
mrmechko <rbose@cs.rochester.edu>
2015-06-23 13:56:27 -0400

    attempt 2 for travis

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-git clone https://github.com/mrmechko/flaming-tyrion.git
+git wget https://github.com/mrmechko/flaming-tyrion/archive/${data}.zip
+unzip flaming-tyrion${data} -d flaming-tyrion
 ls
 echo $STRIPSXMLPATH
