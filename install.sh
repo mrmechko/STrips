67591664387e1a7babb56cbd326cdf7b3694c72b
mrmechko <rbose@cs.rochester.edu>
2015-06-23 15:17:25 -0400

    i need slpuee

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-#git wget https://github.com/mrmechko/flaming-tyrion/archive/${data}.zip
-#unzip flaming-tyrion${data} -d flaming-tyrion
+git wget https://github.com/mrmechko/flaming-tyrion/archive/${data}.zip
+unzip flaming-tyrion${data} -d flaming-tyrion
 echo "strips.XMLSource = `pwd`/flaming-tyrion/lexicon/data/" > src/main/resources/application.conf
