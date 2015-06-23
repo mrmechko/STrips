b198c5a33b0c106728f1a84408610817ccfd8b52
mrmechko <rbose@cs.rochester.edu>
2015-06-23 15:25:12 -0400

    wow. wget isn't a git command, apparently.  jeez

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-git wget https://github.com/mrmechko/flaming-tyrion/archive/${data}.zip
+wget https://github.com/mrmechko/flaming-tyrion/archive/${data}.zip
 unzip flaming-tyrion${data} -d flaming-tyrion
 ls ./flaming-tyrion/lexicon
 echo "strips.XMLSource = `pwd`/flaming-tyrion/lexicon/data/" > src/main/resources/application.conf
