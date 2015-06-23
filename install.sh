cbe0f7d4fe2f07dfa8b2ae9af2c66db2898ad26b
mrmechko <rbose@cs.rochester.edu>
2015-06-23 16:30:39 -0400

    plllssss

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 wget https://github.com/mrmechko/flaming-tyrion/archive/v${data}.zip
 ls
-unzip -q ${data}
+unzip -q v${data}
 ls 
 echo "strips.XMLSource = `pwd`/flaming-tyrion-${data}/lexicon/data/" > src/main/resources/application.conf
 cat src/main/resources/application.conf
