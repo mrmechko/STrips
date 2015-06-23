b43a62b0662bf6c82df051a4dac981a63fabda01
mrmechko <rbose@cs.rochester.edu>
2015-06-23 15:29:35 -0400

    blech

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 wget https://github.com/mrmechko/flaming-tyrion/archive/${data}.zip
-unzip flaming-tyrion${data} -d flaming-tyrion
+unzip flaming-tyrion-${data} -d flaming-tyrion
 ls ./flaming-tyrion/lexicon
 echo "strips.XMLSource = `pwd`/flaming-tyrion/lexicon/data/" > src/main/resources/application.conf
 cat src/main/resources/application.conf
